0dc7c6652fa41922046f5b80fbf05f44ade8858f
JSF14 <157312050+JSF14@users.noreply.github.com>
2024-01-24 10:48:06 +0000

    Update ft_strcat.c

--- ft_strcat.c
+++ ft_strcat.c
@@ -6,12 +6,12 @@ char    *ft_strcat(char *dest, char *src)
     int j;
 
     i = 0;
-    while (dest[i])
+    while (dest[i] != '\0')
     {
         i++;
     }
     j = 0;
-    while (src[j])
+    while (src[j] != '\0')
     {
         dest[i] = src[j];
         i++;
